de60da4aef94d57bb1cbbedb379db8fb2037da94
Arifur999 <arr9677@gmail.com>
2025-05-21 21:29:12 +0600

    added private routs functionality section

--- src/components/PrivateRouts.jsx
+++ src/components/PrivateRouts.jsx
@@ -4,6 +4,15 @@ import {Navigate, useLocation } from 'react-router';
 import Loader from './Loader';
 
 const PrivateRouts = ({children}) => {
+    const {user,loading}=useContext(AuthContext)
+    console.log(loading);
+    const location =useLocation()
+    if(loading){
+        return <Loader></Loader>
+    }
+    if(!user || !user.email){
+        return<Navigate state={{from:location.pathname}} to='/login'></Navigate>
+    }
     return (
         <div>
             {children}
