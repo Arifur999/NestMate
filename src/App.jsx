5b0efed229b22b16e9d38d8921259b124d86e75e
Arifur999 <arr9677@gmail.com>
2025-07-06 13:55:21 +0600

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,15 @@ function App() {
 
   return (
     <>
-      
+      <div className="flex flex-col min-h-screen w-full">
+        <div>
      <Navber></Navber>
+     </div>
+     <div className="flex-grow">
      <Outlet></Outlet>
+     </div>
      <Footer></Footer>
-      
+      </div>
     </>
   )
 }
